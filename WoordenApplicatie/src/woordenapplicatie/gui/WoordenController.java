3f253e76fce9b5e313ecce7259de9073d4bd8c30
Tim Smeets <timsmeets@rocketmail.com>
2014-09-15 13:59:40 +0200

    sorteren op freqentie werkt nu

--- WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
+++ WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.SortedSet;
@@ -79,12 +80,11 @@ public class WoordenController implements Initializable {
         String woordenText = "Totaal aantal woorden: \t\t\t" + words.length + "\n";
 
         HashMap hm = new HashMap();
-        
-        for (int i = 0; i < words.length; i++) 
-        {
+
+        for (int i = 0; i < words.length; i++) {
             hm.put(words[i], i);
         }
-        
+
         woordenText += "Aantal verschillende woorden: \t" + hm.size();
 
         taOutput.setText(woordenText);
@@ -94,90 +94,88 @@ public class WoordenController implements Initializable {
     private void sorteerAction(ActionEvent event) {
         String[] words = splitWords();
         HashMap hm = new HashMap();
-        
-        for (int i = 0; i < words.length; i++) 
-        {
+
+        for (int i = 0; i < words.length; i++) {
             hm.put(words[i], i);
         }
-        
+
         Object[] uniqueWords = hm.keySet().toArray();
         Arrays.sort(uniqueWords);
-        
+
         String output = "";
-        
-        for (int i = uniqueWords.length - 1; i >= 0; i--) 
-        {
+
+        for (int i = uniqueWords.length - 1; i >= 0; i--) {
             output += uniqueWords[i] + "\n";
         }
-        
+
         taOutput.setText(output);
     }
 
     @FXML
     private void frequentieAction(ActionEvent event) {
-        Map totaal;
-        totaal = new TreeMap();
-        String[] uniekwoorden = getUnique();
 
-        for (String s : uniekwoorden) {
-            totaal.put(s, 0);
+        Object[] uniekwoorden = getUnique();
+        ArrayList<Pair> aantal = new ArrayList();
+        HashMap aantallen = new HashMap();
+
+        for (Object s : uniekwoorden) {
+            aantallen.put(s, 0);
         }
         String[] woorden = splitWords();
-        for (String s : woorden) {
-            totaal.replace(s, (int) totaal.get(s) + 1);
-        }
+        
+        for(String s:woorden){
+            int nummer  =  (int) aantallen.get(s);
+            aantallen.replace(s,nummer+1);
+        }        
+
         String output = "";
 
-        ArrayList<Pair> totaal2 = new ArrayList<Pair>();
+        ArrayList<Entry<String,Integer>> totaal2 = new ArrayList<Entry<String,Integer>>();
+        totaal2.addAll( aantallen.entrySet());
         
-        for (String s : uniekwoorden) {
-            totaal2.add(new Pair(s, (Integer) totaal.get(s)));
-        }
-        Collections.sort(totaal2);
-        
-        for (Pair s : totaal2) {
-            
+        Collections.sort(totaal2,new ValueComparator());
+        Collections.reverse(totaal2);
+        for (Entry s : totaal2) {
+
             output += s.getKey() + " " + s.getValue() + "\n";
         }
 
         taOutput.setText(output);
 
     }
+    class ValueComparator implements Comparator<Entry<String,Integer>> {   
 
-    class ValueComparator implements Comparator<String> {
-
-    Map<String, Double> base;
-    public ValueComparator(Map<String, Double> base) {
-        this.base = base;
+        @Override
+        public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+            if(o1.getValue().equals(o2.getValue())){
+                return o1.getKey().compareTo(o2.getKey())*-1;
+            }
+            return o1.getValue().compareTo(o2.getValue());
+        }
     }
 
-    // Note: this comparator imposes orderings that are inconsistent with equals.    
-    public int compare(String a, String b) {
-        if (base.get(a) >= base.get(b)) {
-            return -1;
-        } else {
-            return 1;
-        } // returning 0 would merge keys
-    }
-}
+    
 
     @FXML
     private void concordatieAction(ActionEvent event) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    private String[] getUnique() {
-        Set<String> uniqKeys = new TreeSet<String>();
-        uniqKeys.addAll(Arrays.asList(splitWords()));
-        return uniqKeys.toArray(new String[0]);
+    private Object[] getUnique() {
+        HashMap uniqKeys = new HashMap();
+        String[] woorden = splitWords();
+        for (int i = 0; i < woorden.length; i++) {
+            uniqKeys.put(woorden[i], i);
+        }
+        return uniqKeys.keySet().toArray();
     }
 
     /**
      * Filters all the words and puts them in an array.
+     *
      * @return An array containing all the words from the text.
      */
-    private String[] splitWords() 
-    {
+    private String[] splitWords() {
         String stripped = DEFAULT_TEXT.replace("\n", " ");
         stripped = stripped.replace("  ", " ");
         stripped = stripped.replace(",", "");
@@ -187,3 +185,4 @@ public class WoordenController implements Initializable {
         return words;
     }
 }
+
