8d44d5eb65ed3ae15275e957e64bd00dec31606e
eternalflamez <san_dman@hotmail.com>
2014-09-29 11:38:08 +0200

    opdracht 1 huffman

--- Huffman/JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ Huffman/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -6,17 +6,60 @@
 
 package javaapplication2;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
- * @author Sander_2
+ * @author Sander
  */
 public class JavaApplication2 {
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz 1234567890";
+    static String testString = "";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CreateRandomString();
+        
+        ArrayList<Integer> count = CountCharacters();
+        for (int i = 0; i < count.size(); i++) {
+            System.out.println(alphabet.charAt(i) + " " + count.get(i));
+        }
     }
     
+    private static void CreateRandomString()
+    {
+        for (int i = 0; i < 1000; i++) {
+            Random r = new Random();
+            
+            testString += alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+    }
+    
+    /**
+     * Counts all the characters in the testword.
+     * @return A list with the length of the alphabet variable, with as value the amount of times the letter was found.
+     * Ex: "aap" -> { [0: 2], [1 : 0], [ 2 : 0] ... etc until the index for "p", which has a value of 1.
+     */
+    private static ArrayList<Integer> CountCharacters()
+    {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        for (int i = 0; i < alphabet.length(); i++) 
+        {
+            result.add(0);
+        }
+        
+        testString = testString.toLowerCase();
+        
+        for (int i = 0; i < alphabet.length(); i++) 
+        {
+            int instancesRemoved = testString.length() - testString.replaceAll(Character.toString(alphabet.charAt(i)), "").length();
+            result.set(i, instancesRemoved);
+        }
+        
+        return result;
+    }
 }
